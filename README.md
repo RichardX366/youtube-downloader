ba4908bf167974c14c2d7bdd911a0d0eae32b8cb
Richard <richardx366@gmail.com>
2023-05-06 18:35:14 -0400

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# How to Use
+
+1. Use `yarn && yarn build` to build the project. After building once you don't have to again.
+2. Put `KEY=DEEPL_KEY` and `OPENAI_API_KEY=OPENAI_API_KEY` into a .env file.
+3. Use the command `yarn start VIDEO_IDS` to start it up. VIDEO_IDS is a comma separated list of the video IDs. You can get this from the video URL or for playlists, the below script.
+
+# Playlists
+
+Paste this into browser console and pass in results.
+
+```js
+const links = [];
+document
+  .querySelectorAll('#items #wc-endpoint')
+  .forEach((x) => links.push(x.href.split('=')[1].split('&')[0]));
+console.log(links.join(','));
+```
---
+++ src/index.ts
@@ -0,0 +1,139 @@
+import downloader from 'youtube-mp3-downloader';
+import { Promise as id3 } from 'node-id3';
+import deepL from 'deepl';
+import {
+  renameSync,
+  createWriteStream,
+  unlinkSync,
+  mkdirSync,
+  existsSync,
+} from 'fs';
+import { config } from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
+import axios from 'axios';
+
+config();
+if (!existsSync('./out')) mkdirSync('./out');
+
+let cost = 0;
+const ETAs: { [id: string]: number } = {};
+const songs: {
+  videoId: string;
+  stats: { transferredBytes: number; runtime: number; averageSpeed: number };
+  file: string;
+  youtubeUrl: string;
+  videoTitle: string;
+  artist: string;
+  title: string;
+  thumbnail: string;
+}[] = [];
+
+const YD = new downloader({
+  ffmpegPath: '/usr/local/bin/ffmpeg',
+  outputPath: './out',
+  youtubeVideoQuality: 'highestaudio',
+  queueParallelism: 999,
+  progressTimeout: 1000,
+  allowWebm: true,
+});
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openAI = new OpenAIApi(configuration);
+
+const translate = async (text: string) => {
+  const { data } = await deepL({
+    free_api: true,
+    text,
+    target_lang: 'EN',
+    auth_key: process.env.KEY as string,
+  });
+  return data.translations[0].text;
+};
+
+const prompt = async (text: string) => {
+  const {
+    data: { choices, usage },
+  } = await openAI.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        content: text,
+        role: 'user',
+      },
+    ],
+  });
+  cost += (usage?.total_tokens as number) * 2e-6;
+  const message = choices[0].message?.content as string;
+  return message;
+};
+
+const downloadImage = async (url: string, path: string) => {
+  const { data: response } = await axios({
+    method: 'GET',
+    url,
+    responseType: 'stream',
+  });
+
+  response.pipe(createWriteStream(path));
+  return new Promise<void>((res) => {
+    response.on('end', () => res());
+  });
+};
+
+const logUpdate = (text: string) => {
+  process.stdout.clearLine(0);
+  process.stdout.cursorTo(0);
+  process.stdout.write(text);
+};
+
+YD.on('progress', (progress) => {
+  ETAs[progress.videoId] = progress.progress.eta;
+  logUpdate(`ETA: ${Math.round(Math.max(...Object.values(ETAs)))} seconds`);
+});
+
+YD.on('finished', async (err, data) => {
+  songs.push(data);
+  if (songs.length !== ids.length) return;
+
+  const processedSongs = JSON.parse(
+    await prompt(`Print the pure titles and artists without feat/ft of the following songs in original order and the format [{"title":TITLE,"artist":ARTIST},...]
+${await translate(songs.map((song) => song.videoTitle).join('\n'))}`),
+  ) as [];
+
+  await Promise.all(processedSongs.map(handleSong));
+  logUpdate('Done!\n');
+});
+
+const handleSong = async (
+  song: { title: string; artist: string },
+  i: number,
+) => {
+  const artist = song.artist.split(' feat')[0].split(' ft')[0];
+
+  const title = song.title
+    .split(' ')
+    .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+
+  renameSync(songs[i].file, `./out/${title}.mp3`);
+
+  await downloadImage(songs[i].thumbnail.split('?')[0], `./out/${title}.jpg`);
+
+  await id3.write(
+    {
+      title: title,
+      artist: artist,
+      APIC: `./out/${title}.jpg`,
+    } as any,
+    `./out/${title}.mp3`,
+  );
+
+  unlinkSync(`./out/${title}.jpg`);
+};
+
+const ids = process.argv[2].split(',');
+ids.forEach((id) => YD.download(id));
+
+process.on('beforeExit', () => console.log(`$${cost.toFixed(5)} used`));
